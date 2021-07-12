fddb55b27481070c34bcc358f2b28a419ed36871
f28emi <62606980+f28emi@users.noreply.github.com>
2021-07-12 12:42:48 +0530

    Update urls.py

--- hrmapp/urls.py
+++ hrmapp/urls.py
@@ -7,4 +7,15 @@ urlpatterns = [
     path('register',views.register,name='register'),
     path('login',views.login,name='login'),
     path('logout',views.logout,name='logout'),
+    
+    
+    
+    
+    path('', views.add, name='add'),
+    path('delete/<int:taskid>/', views.delete, name='delete'),
+    path('update/<int:id>/', views.update, name='update'),
+    path('cbvhome/',views.TaskListview.as_view(),name="cbvhome"),
+    path('cbvdetail/<int:pk>/',views.TaskDetailView.as_view(),name='cbvdetail'),
+    path('cbvupdate/<int:pk>/',views.TaskUpdateView.as_view(),name='cbvupdate'),
+    path('cbvdelete/<int:pk>/',views.TaskDeleteView.as_view(),name='cbvdelete')
 ]
